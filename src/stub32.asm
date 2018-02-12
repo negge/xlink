a9998b3f8049f17f4d2c2bb452c2687e3241b33c
Nathan E. Egge <negge@dgql.org>
2018-02-12 09:58:35 -0500

    Add a stub for switching to protected mode.
    stub32.o = 72 bytes

---
+++ src/stub32.asm
@@ -0,0 +1,105 @@
+SEGMENT _TEXT USE32 CLASS=CODE
+
+EXTERN main_
+GLOBAL _xlink_base
+
+  ; Start with 16-bit code
+  bits 16
+
+stub32:
+  ; DX = CS
+  shl edx, 4
+  mov [_xlink_base], edx
+
+  ; AX = 0x1687 get DPMI real mode to protected mode entry point
+  mov ax,0x1687
+  int 0x2f
+
+  ; Test for the presence of a DPMI host, AX = 0 means function successful
+  dec ax
+  js dpmi_ok
+
+  ret
+
+dpmi_ok:
+
+  ; ES:DI = segment:offset of the real-to-protected mode entry point
+  push es
+  push di
+
+  ; AX = 0xffff if DPMI host exists
+  ; BX = 1 if DPMI host supports 32-bit programs
+  xchg ax, bx
+
+  ; SI = number of paragraphs required for DPMI host private data area
+  mov dx, cs
+  inc dh
+  push dx
+  ; This sets ES = CS + 0x100 or 4k past the entry point
+  ; TODO: ensure this doesn't collide with other BSS data
+  pop es
+
+  ; Call the real-to-protected mode entry point with the following paramters:
+  ;  AX = flags, bit 0 should be 1 for 32-bit applications
+  ;  ES = real mode segment of DPMI host private data area
+  call far [esp]
+
+  ; BX = 0xffff
+  push bx
+  push bx
+  pop cx
+  pop dx
+
+  ; DS = selector with base of real mode DS and a 64 KB limit
+  ; ES = selector to program's PSP with a 100H byte limit
+  push ds
+  push es
+
+  ; AX = 1
+  mov bp, ax
+
+@L1:
+  pop bx
+
+  ; AH = 0
+  mov al, 0x8
+
+  ; Sets the segment limit to 4GB
+  ;  AX = 0008h
+  ;  BX = selector
+  ;  CX:DX = 32-bit segment limit
+  int 0x31
+
+  dec bp
+  jnz @L1
+
+  ; AH = 0
+  mov al, 0x9
+
+  ; CS = 16-bit selector with base of real mode CS and a 64KB limit
+  mov bx, cs
+
+  ; CX = 0xffff
+  inc cx
+  mov cl, bl
+  shl cl, 5
+  or cx, 0xC09A
+
+  ; Set the CS segment to allow 32-bit code
+  ;  AX = 0009h
+  ;  BX = selector
+  ;  CX = descriptor access rights
+  int 0x31
+
+  ; Switch to 32-bit code
+  bits 32
+
+  call main_
+
+  mov ah, 0x4c
+  int 0x21
+
+SEGMENT _BSS USE32 CLASS=BSS
+
+_xlink_base:
+  RESD 1
