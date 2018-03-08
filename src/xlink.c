87ffee9dcfdb2859fc035bbfafe4a0ab160ddf08
Nathan E. Egge <negge@dgql.org>
2018-03-07 20:21:01 -0500

    Add an arithmetic range coder.

--- src/xlink.c
+++ src/xlink.c
@@ -2466,6 +2466,8 @@ void xlink_modeler_search(xlink_modeler *mod, xlink_list *models) {
   xlink_modeler_print(mod, models);
 }
 
+#if 0
+
 typedef struct xlink_ans_encoder xlink_ans_encoder;
 
 struct xlink_ans_encoder {
@@ -2640,6 +2642,237 @@ typedef xlink_ans_decoder xlink_decoder;
 #define xlink_decoder_init xlink_ans_decoder_init
 #define xlink_decoder_clear xlink_ans_decoder_clear
 #define xlink_decoder_read_byte xlink_ans_decoder_read_byte
+
+#else
+
+typedef uint32_t xlink_word;
+typedef uint64_t xlink_dword;
+
+typedef struct xlink_range_encoder xlink_range_encoder;
+
+struct xlink_range_encoder {
+  xlink_context *ctx;
+  xlink_list bytes;
+  int bits;
+  xlink_word low;
+  xlink_word range;
+  int zero;
+  int ones;
+};
+
+typedef struct xlink_range_decoder xlink_range_decoder;
+
+struct xlink_range_decoder {
+  xlink_context *ctx;
+  const xlink_list *bytes;
+  int bits;
+  xlink_word low;
+  xlink_word range;
+  xlink_word value;
+};
+
+#define EC_BITS (31)
+#define EC_BASE (1U << EC_BITS)
+#define EC_HALF (EC_BASE >> 1)
+#define EC_MASK (EC_BASE + (EC_BASE - 1))
+
+void xlink_range_encoder_init(xlink_range_encoder *enc, xlink_context *ctx) {
+  enc->ctx = ctx;
+  xlink_list_init(&enc->bytes, sizeof(unsigned char), 0);
+  enc->bits = 0;
+  enc->low = 0;
+  enc->range = EC_BASE;
+  enc->zero = 0;
+  enc->ones = 0;
+}
+
+void xlink_range_encoder_clear(xlink_range_encoder *enc) {
+  xlink_list_clear(&enc->bytes);
+}
+
+#define xlink_range_encoder_store(enc, bit) \
+  do { \
+    xlink_list_expand_capacity(&enc->bytes, (enc->bits & ~7) + 1); \
+    XLINK_SET_BIT(enc->bytes.data, enc->bits, bit); \
+    enc->bits++; \
+  } \
+  while (0)
+
+void xlink_range_encoder_emit(xlink_range_encoder *enc, int bit) {
+  XLINK_ERROR(bit == 1 && enc->zero == 0,
+   ("Got a carry, but have not seen a zero to propogate it into yet"));
+  if (bit == 0) {
+    if (enc->zero) {
+      xlink_range_encoder_store(enc, 0);
+    }
+    while (enc->ones > 0) {
+      xlink_range_encoder_store(enc, 1);
+      enc->ones--;
+    }
+    enc->zero = 1;
+  }
+  else {
+    xlink_range_encoder_store(enc, 1);
+    enc->zero = enc->ones > 0;
+    while (enc->ones > 1) {
+      xlink_range_encoder_store(enc, 0);
+      enc->ones--;
+    }
+    enc->ones = 0;
+  }
+  enc->bytes.length = (enc->bits + 7)/8;
+}
+
+void xlink_range_encoder_write_bit(xlink_range_encoder *enc, xlink_word c0,
+ xlink_word c1, int bit) {
+  xlink_word s;
+  XLINK_ERROR(c0 == 0 || c1 == 0 || (c0 > EC_MASK - c1),
+   ("Error invalid counts, c0 = %i and c1 = %i", c0, c1));
+  s = ((xlink_dword)enc->range)*c0/(c0 + c1);
+  enc->range = bit ? enc->range - s : s;
+  if (bit) {
+    /* Handle carry */
+    if (s > EC_MASK - enc->low) {
+      xlink_range_encoder_emit(enc, 1);
+    }
+    enc->low += s;
+  }
+  /* Normalize */
+  while (enc->range < EC_BASE) {
+    if (enc->low & EC_BASE) {
+      enc->ones++;
+    }
+    else {
+      xlink_range_encoder_emit(enc, 0);
+    }
+    enc->low <<= 1;
+    enc->range <<= 1;
+  }
+}
+
+void xlink_range_encoder_write_bytes(xlink_range_encoder *enc,
+ xlink_list *bytes) {
+  int i, j;
+  for (j = 0; j < xlink_list_length(bytes); j++) {
+    unsigned char byte;
+    unsigned char partial;
+    byte = *xlink_list_get_byte(bytes, j);
+    partial = 1;
+    /* Build partially seen byte from high bit to low bit to match decoder. */
+    for (i = 8; i-- > 0; ) {
+      unsigned int counts[2];
+      int bit;
+      xlink_context_get_counts(enc->ctx, partial, counts);
+      bit = !!(byte & (1 << i));
+      xlink_range_encoder_write_bit(enc, counts[0], counts[1], bit);
+      partial <<= 1;
+      partial |= bit;
+    }
+    XLINK_ERROR(partial != byte,
+     ("Mismatch between partial %02x and byte %02x", partial, byte));
+    xlink_context_update(enc->ctx, byte);
+  }
+}
+
+void xlink_range_encoder_finalize(xlink_range_encoder *enc,
+ xlink_bitstream *bs) {
+  xlink_word m;
+  int s;
+  int i;
+  m = EC_BASE - 1;
+  /* Count how many bits must be output to ensure that the symbols encoded thus
+      far will decode correctly regardless of the bits that follow. */
+  for (s = 1; ((enc->low + m) | m) - enc->low >= enc->range; s++, m >>= 1);
+  XLINK_ERROR(s > 2,
+   ("At most two bits need to be written to finalize range coder"));
+  if (m > EC_MASK - enc->low) {
+    /* Handle carry */
+    xlink_range_encoder_emit(enc, 1);
+  }
+  for (i = 0; i < s; i++) {
+    if ((enc->low + m) & (EC_BASE >> i)) {
+      enc->ones++;
+    }
+    else {
+      xlink_range_encoder_emit(enc, 0);
+    }
+  }
+  /* Flush any pending bits */
+  xlink_range_encoder_emit(enc, 0);
+  /* Copy finalized bits to bitstream */
+  bs->bits = enc->bits;
+  bs->bytes.length = 0;
+  xlink_list_append(&bs->bytes, &enc->bytes);
+}
+
+void xlink_range_decoder_init(xlink_range_decoder *dec, xlink_context *ctx,
+ xlink_bitstream *bs) {
+  dec->ctx = ctx;
+  dec->bytes = &bs->bytes;
+  dec->bits = 1;
+  dec->low = 0;
+  dec->range = 1;
+  dec->value = 0;
+}
+
+void xlink_range_decoder_clear(xlink_range_decoder *dec) {
+}
+
+int xlink_range_decoder_read_bit(xlink_range_decoder *dec, unsigned int c0,
+ unsigned int c1) {
+  int bit;
+  xlink_word s;
+  XLINK_ERROR(c0 == 0 || c1 == 0 || (c0 > EC_MASK - c1),
+   ("Error invalid counts, c0 = %i and c1 = %i", c0, c1));
+  /* Fill value with bits until EC_BASE <= range < 2*EC_BASE */
+  while (dec->range < EC_BASE) {
+    dec->low <<= 1;
+    dec->range <<= 1;
+    dec->value <<= 1;
+    dec->value |= XLINK_GET_BIT(dec->bytes->data, dec->bits);
+    dec->bits++;
+  }
+  s = ((xlink_dword)dec->range)*c0/(c0 + c1);
+  XLINK_ERROR(s == 0 || s >= dec->range, ("Invalid scale value s = %02x", s));
+  bit = dec->value >= s;
+  if (bit) {
+    dec->low += s;
+    dec->range -= s;
+    dec->value -= s;
+  }
+  else {
+    dec->range = s;
+  }
+  return bit;
+}
+
+unsigned char xlink_range_decoder_read_byte(xlink_range_decoder *dec) {
+  unsigned char byte;
+  int i;
+  byte = 1;
+  for (i = 8; i-- > 0; ) {
+    unsigned int counts[2];
+    int bit;
+    xlink_context_get_counts(dec->ctx, byte, counts);
+    bit = xlink_range_decoder_read_bit(dec, counts[0], counts[1]);
+    byte <<= 1;
+    byte |= bit;
+  }
+  xlink_context_update(dec->ctx, byte);
+  return byte;
+}
+
+typedef xlink_range_encoder xlink_encoder;
+#define xlink_encoder_init xlink_range_encoder_init
+#define xlink_encoder_clear xlink_range_encoder_clear
+#define xlink_encoder_write_bytes xlink_range_encoder_write_bytes
+#define xlink_encoder_finalize xlink_range_encoder_finalize
+typedef xlink_range_decoder xlink_decoder;
+#define xlink_decoder_init xlink_range_decoder_init
+#define xlink_decoder_clear xlink_range_decoder_clear
+#define xlink_decoder_read_byte xlink_range_decoder_read_byte
+
+#endif
 
 void xlink_bitstream_from_context(xlink_bitstream *bs, xlink_context *ctx,
  xlink_list *bytes) {
