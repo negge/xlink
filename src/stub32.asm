585e9bb798933d97cbe75c5061a0ee53efc58d7d
Nathan E. Egge <negge@dgql.org>
2018-12-21 08:52:08 -0500

    Add structure to reference 32-bit stack registers.

--- src/stub32.asm
+++ src/stub32.asm
@@ -1,3 +1,15 @@
+struc stack
+  .edi: resd 1
+  .esi: resd 1
+  .ebp: resd 1
+  .esp: resd 1
+  .ebx: resd 1
+  .edx: resd 1
+  .ecx: resd 1
+  .eax: resd 1
+  .size:
+endstruc
+
 ;%define DEBUG
 %include 'debug.inc'
 
