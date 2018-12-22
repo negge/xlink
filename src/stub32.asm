e11dab09b022976d45b18a7ffb64c399d4fb1e34
Nathan E. Egge <negge@dgql.org>
2018-12-22 05:04:03 -0500

    Add XLINK_STUB_PACK to enable binary decompression.
    This commit adds the PAQ decompression algorithm to the 32-bit stub
     behind the XLINK_STUB_PACK define (default disabled).
    Many assumptions are made including:
     - size of the hash table is fixed at 6MB
     - number of 4k pages to skip before the DPMI host scratch area
     - number of model parameters in the DATA segment is 9
     - parity of branching direction when finished decoding is hard coded
     - no overflow checking is done when updating counts
     - disabled 16-bit initializer
     - initial adaptation rate when no data is seen is fixed

--- src/stub32.asm
+++ src/stub32.asm
@@ -17,13 +17,25 @@ endstruc
 %define XLINK_STUB_INIT 0
 %endif
 
+%ifndef XLINK_STUB_PACK
+%define XLINK_STUB_PACK 0
+%endif
+
+%if XLINK_STUB_PACK
+%define HASH_TABLE_SIZE (6*1024*1024)
+%define HASH_TABLE_SEGS (HASH_TABLE_SIZE/65536)
+%define HASH_TABLE_WORDS (HASH_TABLE_SIZE/2)
+%endif
+
 CPU 386
 
 %if XLINK_STUB_INIT
 EXTERN init_
 %endif
 
+%if !XLINK_STUB_PACK
 EXTERN main_
+%endif
 
 SEGMENT _MAIN USE32 CLASS=CODE
 
@@ -52,6 +64,10 @@ dpmi_ok:
 
   ASSERT {test bx, 1}, NZ, 'DPMI host missing 32-bit support.'
 
+%if XLINK_STUB_PACK
+  push cs
+%endif
+
   ; ES:DI = segment:offset of the real-to-protected mode entry point
   push es
   push di
@@ -77,14 +93,41 @@ dpmi_ok:
   ASSERT {}, NC, 'Failed to enter DPMI protected mode.'
 
   ; BX = 0xffff
-  mov cx, bx
   mov dx, bx
+
+%if XLINK_STUB_PACK
+
+  push ax
+
+  ; AL = 1
+  mov ah, 5
+  mov bx, HASH_TABLE_SEGS
+
+  ; Call allocate memory block
+  ;  AX = 0501h
+  ;  BX:CX = Size of memory block to allocate in bytes
+  int 0x31
+
+  ASSERT {}, NC, 'Failed to allocate memory'
+
+  mov ax, 8
+  push ax
+
+  ; BX:CX = linear address of allocated memory block
+  push bx
+  push cx
+
+%else
+
+  ; AH = 0
+  mov al, 8
+
+%endif
 
   ; ES = selector to program's PSP with a 100H byte limit
   mov bx, es
 
-  ; AH = 0
-  mov al, 0x8
+  mov cx, dx
 
   ; Sets the segment limit to 4GB
   ;  AX = 0008h
@@ -103,6 +146,8 @@ dpmi_ok:
   ; CS = 16-bit selector with base of real mode CS and a 64KB limit
   mov bx, cs
 
+  ASSERT {}, NC, 'Failed to set 4 GB segment limit for CS.'
+
   ; Load access rights into CH
   lar cx, bx
   ; Set page granular (10000000) and 32-bit (01000000) extended access rights
@@ -120,7 +165,289 @@ dpmi_ok:
 
   ASSERT {}, NC, 'Failed to change code segment to 32-bit.'
 
+%if !XLINK_STUB_PACK
+
   call main_
 
   mov ah, 0x4c
   int 0x21
+
+%else
+  ; Set ESI to the start of the EC header (minus models in first EC segment)
+  mov esi, ec_segs - 0x9
+
+  ; Pop the zero based address of the allocated memory
+  pop eax
+
+  ; Pop the address 0x10008
+  pop edi
+
+  ; Pop the real-to-protected mode entry point (and ignore it)
+  pop ebx
+
+  ; Pop the real mode 0:cs and convert to a zero based address
+  pop ebx
+  shl ebx, 4
+
+  ; Compute and store the hashtable address relative to ES
+  sub eax, ebx
+  mov [esi + hash_table_instr + 1 - stub32_end + 0x9], eax
+
+  ; Start by clearing the memory
+  ;  edi = 0x10008
+  mov ecx, edi
+  xor eax, eax
+
+  ; Manually clear the first 8 bytes
+  stosd
+  stosd
+
+  ; EDI = 0x10008 + 0x8 is the entry point of the binary
+  push edi
+  ; EBP is the starting bit index and must be 0
+  xor ebp, ebp
+  ; EAX is the starting range and must be 1
+  inc eax
+  pushad
+  dec eax
+  ; Clear 256kB of memory
+  rep stosd
+
+  ; Hash table clear routine expects carry to be set
+  stc
+  jmp @clear_hash_table
+
+
+
+@done_model:
+
+  ; if EBX == 0 or EBX > 0x7fffffff (EBX = 0 or -1 if we've decoded a bit)
+  add ebx, ebx
+  popad
+  ; ZF = 0 and SF = OF
+  jg @decode_bit
+
+;  ; if EBX == 0 or EBX > 0x7fffffff (EBX = 0 or -1 if we've decoded a bit)
+;  dec ebx
+;  popad
+;  ; ZF = 0 and SF = OF
+;  jns @decode_bit
+
+@init_byte:
+  rcl byte [edi], 1
+  jnc @next_bit
+  inc edi
+  jmp @init_byte
+
+
+
+; Load a bit into the range coder
+;  EBP = bit index
+;  EAX = range
+;  ECX = base
+@need_bit:
+
+  ; Read the bit into carry flag
+  bt [ec_bits], ebp
+
+  ; Update base: ECX = (base << 1) + bit
+  adc ecx, ecx
+
+  ; Update range: EAX = range << 1
+  shl eax, 1
+
+  inc ebp
+
+; Decode a range coded bit using computed counts
+;  EAX = range
+;  ECX = base
+;  EBX = counts for 0
+;  EDX = counts for 1
+;  EBP = next bit index
+@decode_bit:
+
+  ; While EAX < 0x8000000, need a bit
+  test eax, eax
+  jns @need_bit
+
+  ; Compute c0 + c1
+  ;  EBX = c0
+  ;  EDX = c1
+  add ebx, edx
+
+  ; EAX = range
+  push eax
+
+  ; Compute s = range*p0 = range*c0/(c0 + c1)
+  ;  EAX = range
+  ;  EDX = c0
+  ;  EBX = c0 + c1
+  mul edx
+  div ebx
+
+  ; [ESP] = range
+  pop edx
+
+  ; Decoded bit = 0 if base < s
+  cmp ecx, eax
+  ; Set ebx = 0 or -1
+  sbb ebx, ebx
+
+  jc @next_bit
+
+  ; EAX = s
+  ; EDX = range
+  xchg eax, edx
+
+  ; ECX = base
+  ; EDX = s
+  sub ecx, edx
+
+  ; EAX = range
+  ; EDX = s
+  sub eax, edx
+
+  ;jmp @next_bit
+
+
+
+@next_bit:
+
+  ; ebp = bit index
+  ; eax = range
+  ; ecx = value
+  ; ebx = c0 count
+  ; edx = c1 count
+  ; edi = output bytes
+  ; esi = EC segment
+  pushad
+
+  ; ESI = start of EC segment
+  ; [ESI] encodes the length of the EC segment as -EDI when done
+  lodsd
+
+  ; EAX = -(start address + number of bytes)
+  ; EDI = address of currently decoding byte
+  add eax, edi
+
+  ; EAX = 0 when we are done with this EC segment
+  jz @clear_hash_table
+
+  ; Reset the counts (initial value changes adaptation rate)
+  push 2
+  pop edx
+  mov [esp + stack.edx], edx
+  ;mov [esp + stack.ebx], edx
+  mov [esp + edx*8], edx
+
+  ; [ESI] encodes the number of models and their weights
+  lodsd
+  xor ebp, ebp
+@next_model:
+  dec ebp
+@double_weight:
+  inc ebp
+  add eax, eax
+  jc @double_weight
+  jz @done_model
+
+  pushad
+
+  ; Load the model
+  lodsb
+
+  mov dl, al
+@hash_byte:
+  ; EDI = location of byte we are decoding on first pass
+  ; [EDI] = partially decoded byte or previously decoded byte
+  xor al, [edi]
+  imul eax, 0x6f
+  ;rol eax, 9
+  add al, [edi]
+  dec eax
+@skip_byte:
+  dec edi
+  add dl, dl
+  jc @hash_byte
+  jnz @skip_byte
+
+@clear_hash_table:
+
+hash_table_instr:
+  mov edi, 0
+  mov ecx, HASH_TABLE_WORDS
+  jnc @index_hash
+
+  rep stosw
+
+  or al, [esi]
+  popad
+
+  ;TODO need to hardcode this value 8 bytes + n model bytes
+  lea esi, [esi + 0x9]
+  ;TODO we can flip this (and all of the weight dwords) if ec_seg - XXX is odd
+  jpo @next_bit
+
+@done_decoding:
+
+  ; [ESP] = 0x10010
+  ret
+
+
+
+@index_hash:
+
+  ; Compute the remainder to index the hash table
+  div ecx
+
+  ; EDX = computed hash
+  ; EDI = hash table offset
+  lea edi, [edi + 2*edx]
+
+  ;TODO don't recompute EDI and instead just store it
+  ;store edi somewhere indexed by ESI
+  ;mov [ebp + 4*esi], edi
+  ;mov edi, [ebp + 4*esi]
+
+  ; Accumulate weighted counts for model
+
+  ; EBP = model weight
+  mov ecx, ebp
+
+  ; EDI = hash index
+  ; ECX = model weight
+  xor eax, eax
+@load_counts:
+  movzx edx, byte [edi + eax]
+  shl edx, cl
+  ; Index stack to update EDX (when EAX = 0) and EBX (when EAX = -1)
+  add [esp + eax*4 + stack.size + stack.edx], edx
+  dec eax
+  jp @load_counts
+
+  test ebx, ebx
+  jg @skip_update
+  shr byte [edi + ebx], 1
+  jnz @floor_fine
+  rcl byte [edi + ebx], 1
+@floor_fine:
+  not ebx
+  inc byte [edi + ebx]
+
+@skip_update:
+  popad
+  inc esi
+  jmp @next_model
+
+
+
+stub32_end:
+
+SEGMENT _PROG USE16 CLASS=BSS
+
+GLOBAL ec_segs
+GLOBAL ec_bits
+
+ec_segs: resb 9
+ec_bits:
+%endif
