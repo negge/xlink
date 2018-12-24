7817ac15e5d0a2cbfe34b7a851fb075786d52a4e
Nathan E. Egge <negge@dgql.org>
2018-12-24 09:33:25 -0500

    Add 16-bit safe print_hexXX functions and macros.
    The PRINTX macros now detect if we are in 16-bit or 32-bit CODE and
     dispatch to the correct debug print_hexXX functions.

--- src/debug.asm
+++ src/debug.asm
@@ -1,17 +1,20 @@
 CPU 386
 
-BITS 32
-
-GLOBAL print_hex4
-GLOBAL print_hex8
-GLOBAL print_hex16
-GLOBAL print_hex32
 GLOBAL print_string_16bit
 GLOBAL print_string_32bit
+GLOBAL print_hex4
+GLOBAL print_hex8_16bit
+GLOBAL print_hex16_16bit
+GLOBAL print_hex32_16bit
+GLOBAL print_hex8_32bit
+GLOBAL print_hex16_32bit
+GLOBAL print_hex32_32bit
 
 SEGMENT _TEXT5 USE16 CLASS=CODE
 
 print_string_16bit:
+; Print a string using DOS INT 21h, subfunction 9h
+;  DX = pointer to $ terminated string
   pusha
   push ds
   push cs
@@ -67,11 +70,51 @@ print_hex4:
   popa
   ret
 
+BITS 16
+
+SEGMENT _TEXT6 USE16 CLASS=CODE
+
+; Print an 8-bit hexadecimal number
+;  AL = number
+print_hex8_16bit:
+  push ax
+  shr ax, 4
+  call print_hex4
+  pop ax
+  call print_hex4
+  ret
+
+SEGMENT _TEXT7 USE16 CLASS=CODE
+
+; Print a 16-bit hexadecimal number
+;  AX = number
+print_hex16_16bit:
+  push ax
+  xchg al, ah
+  call print_hex8_16bit
+  pop ax
+  call print_hex8_16bit
+  ret
+
+SEGMENT _TEXT8 USE16 CLASS=CODE
+
+; Print a 32-bit hexadecimal number
+;  EAX = number
+print_hex32_16bit:
+  push eax
+  shr eax, 16
+  call print_hex16_16bit
+  pop eax
+  call print_hex16_16bit
+  ret
+
+BITS 32
+
 SEGMENT _TEXT1 USE16 CLASS=CODE
 
 ; Print an 8-bit hexadecimal number
 ;  AL = number
-print_hex8:
+print_hex8_32bit:
   push eax
   shr eax, 4
   call print_hex4
@@ -83,22 +126,22 @@ SEGMENT _TEXT2 USE16 CLASS=CODE
 
 ; Print a 16-bit hexadecimal number
 ;  AX = number
-print_hex16:
+print_hex16_32bit:
   push eax
   xchg al, ah
-  call print_hex8
+  call print_hex8_32bit
   pop eax
-  call print_hex8
+  call print_hex8_32bit
   ret
 
 SEGMENT _TEXT3 USE16 CLASS=CODE
 
 ; Print a 32-bit hexadecimal number
 ;  EAX = number
-print_hex32:
+print_hex32_32bit:
   push eax
   shr eax, 16
-  call print_hex16
+  call print_hex16_32bit
   pop eax
-  call print_hex16
+  call print_hex16_32bit
   ret
