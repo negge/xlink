a9a9521fb5da91251509bc0a48f029cad2b0bef3
Nathan E. Egge <negge@dgql.org>
2019-06-17 10:36:11 -0400

    Move OMF parsing definitions into omf.h.

---
+++ src/omf.h
@@ -0,0 +1,133 @@
+#ifndef _XLINK_omf_h
+#define _XLINK_omf_h
+
+typedef enum {
+  OMF_THEADR  = 0x80,  // Translator Header Record
+  OMF_LHEADR  = 0x82,  // Library Module Header Record
+  OMF_MODEND  = 0x8A,  // (0x8B) Module End Record
+  OMF_EXTDEF  = 0x8C,  // External Names Definition Record
+  OMF_PUBDEF  = 0x90,  // (0x91) Public Names Definition Record
+  OMF_FIXUPP  = 0x9C,  // (0x9D) Fixup Record
+  OMF_GRPDEF  = 0x9A,  // Group Definition Record
+  OMF_LNAMES  = 0x96,  // List of Names Record
+  OMF_SEGDEF  = 0x98,  // (0x99) Segment Definition Record
+  OMF_LEDATA  = 0xA0,  // (0xA1) Logical Enumerated Data Record
+  OMF_LIDATA  = 0xA2,  // (0xA3) Logical Iterated Data Record
+  OMF_COMDEF  = 0xB0,  // Communal Names Definition Record
+  OMF_LEXTDEF = 0xB4,  // Local External Names Definition Record
+  OMF_LPUBDEF = 0xB6,  // (0xB7) Local Public Names Definition Record
+  OMF_CEXTDEF = 0xBC,  // COMDAT External Names Definition Record
+  OMF_COMDAT  = 0xC2,  // (0xC3) Initialized Communal Data Record
+  OMF_LLNAMES = 0xCA,  // Local Logical Names Definition Record
+  OMF_LIBHDR  = 0xF0,  // Library Header Record
+  OMF_LIBEND  = 0xF1,  // Library End Record
+} xlink_omf_record_type;
+
+typedef enum {
+  OMF_DATA_TYPE_FAR  = 0x61,
+  OMF_DATA_TYPE_NEAR = 0x62
+} xlink_omf_data_type;
+
+typedef enum {
+  OMF_FRAME_SEG  = 0,
+  OMF_FRAME_GRP  = 1,
+  OMF_FRAME_EXT  = 2,
+  OMF_FRAME_ABS  = 3,
+  OMF_FRAME_LOC  = 4,
+  OMF_FRAME_TARG = 5,
+  OMF_FRAME_FLAT = 6,
+} xlink_omf_frame;
+
+typedef enum {
+  OMF_TARGET_SEG_DISP = 0,  // Segment index with displacement
+  OMF_TARGET_GRP_DISP = 1,  // Group index with displacement
+  OMF_TARGET_EXT_DISP = 2,  // Extern index with displacement
+  OMF_TARGET_ABS_DISP = 3,  // Absolute frame number with displacement
+  OMF_TARGET_SEG      = 4,  // Segment index, no displacement
+  OMF_TARGET_GRP      = 5,  // Group index, no displacement
+  OMF_TARGET_EXT      = 6,  // Extern index, no displacement
+  OMF_TARGET_ABS      = 7,  // Absolute frame number, no displacement
+} xlink_omf_target;
+
+typedef enum {
+  OMF_LOCATION_8BIT          =  0,  // 8-bit or low byte of 16-bit offset
+  OMF_LOCATION_16BIT         =  1,  // 16-bit offset
+  OMF_LOCATION_SEGMENT       =  2,  // 16-bit base
+  OMF_LOCATION_FAR           =  3,  // 16-bit base + 16 big segment
+  OMF_LOCATION_8BIT_HIGH     =  4,  // 8-bit or high byte 16-bit offset
+  OMF_LOCATION_16BIT_LOADER  =  5,  // 16-bit, loader resolved
+  OMF_LOCATION_48BIT_PHARLAP =  6,  // 16-bit base + 32-bit offset, pharlap
+  OMF_LOCATION_32BIT         =  9,  // 32-bit offset
+  OMF_LOCATION_48BIT         = 11,  // 16-bit base + 32-bit offset
+  OMF_LOCATION_32BIT_LOADER  = 13,  // 32-bit, loader resolved
+} xlink_omf_location;
+
+typedef enum {
+  OMF_MODE_RELATIVE = 0,
+  OMF_MODE_EXPLICIT = 1
+} xlink_omf_relocation_mode;
+
+typedef enum {
+  OMF_SEGMENT_CODE,
+  OMF_SEGMENT_DATA,
+  OMF_SEGMENT_BSS
+} xlink_segment_class;
+
+typedef enum {
+  OMF_SEGMENT_ABS,
+  OMF_SEGMENT_BYTE,
+  OMF_SEGMENT_WORD,
+  OMF_SEGMENT_PARA,
+  OMF_SEGMENT_PAGE,
+  OMF_SEGMENT_DWORD
+} xlink_segment_align;
+
+typedef enum {
+  OMF_SEGMENT_USE16,
+  OMF_SEGMENT_USE32
+} xlink_segment_use;
+
+typedef union xlink_omf_attribute xlink_omf_attribute;
+
+union xlink_omf_attribute {
+  uint8_t b;
+  struct {
+    /* Packed byte with segment attributes */
+    uint8_t proc:1, big:1, combine:3, align:3;
+  };
+};
+
+typedef union xlink_omf_fixup_thread xlink_omf_fixup_thread;
+
+union xlink_omf_fixup_thread {
+  uint8_t b;
+  struct {
+    /* Packed byte with FIXUP thread data, high = 0 always */
+    uint8_t thread:2, method:3, is_frame:1, high:1;
+  };
+};
+
+typedef union xlink_omf_fixup_locat xlink_omf_fixup_locat;
+
+union xlink_omf_fixup_locat {
+  struct {
+    uint8_t b1;
+    uint8_t b0;
+  };
+  struct {
+    /* Packed byte with FIXUP location data, high = 1 always */
+    uint16_t offset:10, location:4, mode:1, high:1;
+  };
+};
+
+typedef union xlink_omf_fixup_fixdata xlink_omf_fixup_fixdata;
+
+union xlink_omf_fixup_fixdata {
+  uint8_t b;
+  struct {
+    /* Packed byte with FIXUP fixdata data */
+    uint8_t target:2, no_disp:1, th_target:1, frame:3, th_frame:1;
+  };
+};
+
+#endif
